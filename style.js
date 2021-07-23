3804024480994990615a51ff52416a0091a70db0
Rodrigo Sbampato <77363759+rodrigosbampato1@users.noreply.github.com>
2021-07-23 18:55:31 -0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,100 @@
+import{StyleSheet} from 'react-native';
+
+
+export const styles = StyleSheet.create({
+    content:{
+        backgroundColor:'#696969',
+        height:420,
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:100,
+        marginTop:50,
+        
+        
+    },
+    input:{
+        padding:10,
+        borderWidth:2,
+        fontSize:16,
+        width:300,
+        borderRadius:20,
+        justifyContent:'center',
+        alignItems:'center',
+        textAlign:'center',
+        backgroundColor:"#fdf5e6"
+
+    },
+    botao:{
+       
+        flexDirection:'row',
+        padding:10,
+        alignItems:'center',
+        margin:5,
+        justifyContent: 'space-between',
+        width:220,
+        
+        
+        
+        
+
+
+        
+        
+        
+
+    },
+    
+    
+    header:{
+        backgroundColor:'black',
+        height:100,
+        alignItems:'center',
+        justifyContent:'center',
+        
+        
+    },
+    text:{
+        fontSize:22,
+        backgroundColor:'#d3d3d3',
+        borderRadius:20,
+        display:'flex',
+        alignItems:'center',
+        width:250,
+        padding:5,
+        textAlign:'center',
+        margin:5
+        
+    },
+    text1:{
+        fontSize:30,
+        color:'white'
+    },
+
+    footer:{
+        height:70,
+        backgroundColor:'black',
+        alignItems:'center',
+        marginTop:50
+    },
+    text2:{
+        fontSize:12,
+        color:'white'
+    },
+    
+
+       
+        
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+})
